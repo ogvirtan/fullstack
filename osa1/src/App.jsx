bf474768d9d40c7a1287243231799ccb341102ca
ogvirtan <ovirtanen96@gmail.com>
2025-06-05 14:39:36 +0300

    osa1.3

--- osa1/src/App.jsx
+++ osa1/src/App.jsx
@@ -30,18 +30,24 @@ const Total = (props)=>{
 }
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content osa1={part1} tehtava1={exercises1} osa2={part2} tehtava2={exercises2} osa3={part3} tehtava3={exercises3}/>
-      <Total e1={exercises1} e2={exercises2} e3={exercises3} />
+      <Content osa1={part1.name} tehtava1={part1.exercises} osa2={part2.name} tehtava2={part2.exercises} osa3={part3.name} tehtava3={part3.exercises}/>
+      <Total e1={part1.exercises} e2={part2.exercises} e3={part3.exercises} />
     </div>
   )
 }
