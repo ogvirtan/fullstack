6a0590169af440aaa7160529be7ead5146035d4c
ogvirtan <ovirtanen96@gmail.com>
2025-06-10 17:30:43 +0300

    osa1.6

--- osa1/src/App.jsx
+++ osa1/src/App.jsx
@@ -1,65 +1,27 @@
-const Header = (props) =>{
-  return(
-  <>
-    <h1>{props.course}</h1>
-  </>
-  )
-}
-const Content = (props)=>{
-  return(
-  <>
-    <Part item = {props.lista[0]}/>
-    <Part item = {props.lista[1]}/>
-    <Part item = {props.lista[2]}/>
-  </>
-  )
-}
-const Part = (props)=>{
-  return(
-  <>
-    <p>{props.item.name} {props.item.exercises}</p>
-  </>
-  )
-}
-const Total = (props)=>{
-  function sum(l){
-    let rval = 0
-    l.forEach(element => {
-      rval += element.exercises
-    })
-    return rval
-  }
-  return(
-  <>
-    <p>Number of exercises sum {sum(props.lista)}</p>
-  </>
-  )
-}
+import { useState } from 'react'
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const Display = props => <div>{props.name} {props.value}</div>
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
     <div>
-      <Header course = {course.name} />
-      <Content lista = {course.parts} />
-      <Total lista = {course.parts} />
+      <h1>give feedback</h1>      
+      <Button onClick = {()=>setGood(good+1)} text = "good" />
+      <Button onClick = {()=>setNeutral(neutral+1)} text = "neutral" />
+      <Button onClick = {()=>setBad(bad+1)} text = "bad" />
+      <h2>statistics</h2>
+      <Display value = {good} name = "good"/>
+      <Display value = {neutral} name = "neutral"/>
+      <Display value = {bad} name = "bad"/>
     </div>
-    )
+  )
 }
 
 export default App
