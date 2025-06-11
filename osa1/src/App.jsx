ded7ed3e7ab00597009f0cbf7dbf240cf532ef6b
ogvirtan <ovirtanen96@gmail.com>
2025-06-11 12:44:51 +0300

    osa1.12

--- osa1/src/App.jsx
+++ osa1/src/App.jsx
@@ -2,60 +2,25 @@ import { useState } from 'react'
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
-const Statisticline = (props) => {
-  return(
-  <tr>
-    <td>{props.data.name}</td><td>{props.data.value} {props.data.sc}</td>
-  </tr>
-  )
-}
-
-const Statistics = (props) =>{
-  const t = props.taul
-  let allValueCount = t[3].value
-  if (allValueCount === 0){
-    return(
-      <div>
-        <h2>statistics</h2>
-          <p>no feedback given</p>
-      </div>
-    )
-  }
-  return(  
-    <div>
-      <h2>statistics</h2>
-        <table>
-          <tbody>
-          <Statisticline data={t[0]} />
-          <Statisticline data={t[2]} />
-          <Statisticline data={t[1]} />
-          <Statisticline data={t[3]} />
-          <Statisticline data={t[4]} />
-          <Statisticline data={t[5]} />
-          </tbody>
-        </table>
-    </div>
-  )
-}
-
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  let sum = good+bad+neutral
-  let avg = (good-bad)/sum
-  let pos = (good/sum)*100
-
-  const taulukko = [{value:good, name:"good"}, {value:bad, name:"bad"}, {value:neutral, name:"neutral"}, {value:sum, name:"all"}, {value:avg, name:"average"}, {value:pos, name:"positive", sc:"%"}]
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
 
   return (
     <div>
-      <h1>give feedback</h1>      
-      <Button onClick = {()=>setGood(good+1)} text = "good" />
-      <Button onClick = {()=>setNeutral(neutral+1)} text = "neutral" />
-      <Button onClick = {()=>setBad(bad+1)} text = "bad" />
-      <Statistics taul={taulukko}/>
+      {anecdotes[selected]}
+      <br/>
+      <Button onClick={() => setSelected(Math.floor(Math.random()*(anecdotes.length)))} text="next anecdote" />
     </div>
   )
 }
