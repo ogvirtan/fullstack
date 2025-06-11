16b2d0b6cf0de7204ce436d9a53f9e4705620e54
ogvirtan <ovirtanen96@gmail.com>
2025-06-11 12:14:23 +0300

    osa1.10

--- osa1/src/App.jsx
+++ osa1/src/App.jsx
@@ -2,13 +2,46 @@ import { useState } from 'react'
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
-const Display = props => <div>{props.name} {props.value}</div>
+const Statisticline = (props) => {
+  return(
+  <div>{props.data.name} {props.data.value} {props.data.sc}</div>
+  )
+}
+
+const Statistics = (props) =>{
+  const t = props.taul
+  let allValueCount = t[3].value
+  if (allValueCount === 0){
+    return(
+      <div>
+      <h2>statistics</h2>
+      <p>no feedback given</p>
+      </div>
+    )
+  }
+  return(  
+    <div>
+    <h2>statistics</h2>
+    <Statisticline data={t[0]} />
+    <Statisticline data={t[2]} />
+    <Statisticline data={t[1]} />
+    <Statisticline data={t[3]} />
+    <Statisticline data={t[4]} />
+    <Statisticline data={t[5]} />
+    </div>
+  )
+}
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  let sum = good+bad+neutral
+  let avg = (good-bad)/sum
+  let pos = (good/sum)*100
+
+  const taulukko = [{value:good, name:"good"}, {value:bad, name:"bad"}, {value:neutral, name:"neutral"}, {value:sum, name:"all"}, {value:avg, name:"average"}, {value:pos, name:"positive", sc:"%"}]
 
   return (
     <div>
@@ -16,10 +49,7 @@ const App = () => {
       <Button onClick = {()=>setGood(good+1)} text = "good" />
       <Button onClick = {()=>setNeutral(neutral+1)} text = "neutral" />
       <Button onClick = {()=>setBad(bad+1)} text = "bad" />
-      <h2>statistics</h2>
-      <Display value = {good} name = "good"/>
-      <Display value = {neutral} name = "neutral"/>
-      <Display value = {bad} name = "bad"/>
+      <Statistics taul={taulukko}/>
     </div>
   )
 }
