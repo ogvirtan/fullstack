e76c9b887fba1b331b0b15bfdfb34d3b990bad7a
ogvirtan <ovirtanen96@gmail.com>
2025-07-09 12:41:59 +0300

    uibuild

--- osa3/puhelinluettelon_backend/models/contact.js
+++ osa3/puhelinluettelon_backend/models/contact.js
@@ -16,8 +16,15 @@ mongoose
   });
 
 const contactSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
+  },
 });
 
 contactSchema.set("toJSON", {
