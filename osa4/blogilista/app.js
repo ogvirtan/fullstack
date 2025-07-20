8cf80b1764c875fa9609ba175cb68fed698a1f8d
ogvirtan <ovirtanen96@gmail.com>
2025-07-20 13:10:39 +0300

    osat4.8-4.12

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,23 +1,34 @@
 const express = require('express')
-
+require('express-async-errors')
 const app = express()
 
 const Blog = require('./models/blog')
 
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
+app.get('/api/blogs', async (request, response) => {
+  const blogs = await Blog.find({})
+  if (blogs) {
     response.json(blogs)
-  })
+  } else {
+    response.status(404).end()
+  }
+
 })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
+app.post('/api/blogs', async (request, response) => {
+  const body = request.body
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes ? body.likes : 0
   })
+  if (blog.title && blog.url) {
+    const addedBlog = await blog.save()
+    response.status(201).json(addedBlog)
+  }
+  response.status(400).json(blog)
 })
 
 module.exports = app
---
+++ osa4/blogilista/tests/blog_api.test.js
@@ -0,0 +1,92 @@
+const assert = require('node:assert')
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+test('id field within blogs is valid', async () => {
+  const blogs = await helper.blogsInDb()
+
+  assert(Object.keys(blogs[0]).includes('id'))
+})
+
+test('a valid blog entry is present in the database', async () => {
+  const newBlog = {
+    title: 'testBook',
+    author: 'testAuthor',
+    url: 'test_url',
+    likes: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+  const latestEntry = blogsAtEnd.slice(-1)[0]
+
+  assert.strictEqual(latestEntry.title, newBlog.title)
+  assert.strictEqual(latestEntry.author, newBlog.author)
+  assert.strictEqual(latestEntry.url, newBlog.url)
+  assert.strictEqual(latestEntry.likes, newBlog.likes)
+})
+
+test('a blog entry with no likes-field is present in the database with likes-value set at 0', async () => {
+  const newBlog = {
+    title: 'testBook',
+    author: 'testAuthor',
+    url: 'test_url',
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  const latestEntry = blogsAtEnd.slice(-1)[0]
+  assert.strictEqual(latestEntry.likes, 0)
+})
+
+test('a blog entry with no title or no url returns 400', async () => {
+  const newBlog = {
+    author: 'testAuthor',
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
