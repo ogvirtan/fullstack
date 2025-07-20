fee5e5bd59b0215b9abb2f89f2db8f01c5fb246c
ogvirtan <ovirtanen96@gmail.com>
2025-07-20 15:56:45 +0300

    osat4.13-4.14

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -22,6 +22,19 @@ app.post('/api/blogs', async (request, response) => {
   response.status(201).json(addedBlog)
 })
 
+app.put('/api/blogs/:id', async (request, response) => {
+  const body = request.body
+  const updatedBlog = await Blog.findById(request.params.id)
+  updatedBlog.likes = body.likes
+  await updatedBlog.save()
+  response.status(201).json(updatedBlog)
+})
+
+app.delete('/api/blogs/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
@@ -29,12 +42,15 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const errorHandler = async (error, request, response, next) => {
-  console.error(error.message)
-
   if (error.name === 'ValidationError') {
     return response.status(400).send({ error: 'ValidationError' })
   }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  return response.status(500).send({ error: error.name })
 }
+
 
 app.use(errorHandler)
 
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -1,5 +1,5 @@
 const assert = require('node:assert')
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -13,77 +13,136 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('GET', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
 })
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+describe('POST', () => {
+  test('a valid blog entry is present in the database', async () => {
+    const newBlog = {
+      title: 'testBook',
+      author: 'testAuthor',
+      url: 'test_url',
+      likes: 0
+    }
 
-  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+    const latestEntry = blogsAtEnd.slice(-1)[0]
+
+    assert.strictEqual(latestEntry.title, newBlog.title)
+    assert.strictEqual(latestEntry.author, newBlog.author)
+    assert.strictEqual(latestEntry.url, newBlog.url)
+    assert.strictEqual(latestEntry.likes, newBlog.likes)
+  })
+
+  test('a blog entry with no likes-field is present in the database with likes-value set at 0', async () => {
+    const newBlog = {
+      title: 'testBook',
+      author: 'testAuthor',
+      url: 'test_url',
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    const latestEntry = blogsAtEnd.slice(-1)[0]
+    assert.strictEqual(latestEntry.likes, 0)
+  })
+
+  test('a blog entry with no title or no url returns 400', async () => {
+    const newBlog = {
+      author: 'testAuthor',
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })
 })
 
-test('id field within blogs is valid', async () => {
-  const blogs = await helper.blogsInDb()
+describe('DELETE', () => {
+  test('deleting an item actually deletes an item with status code 204', async () => {
+    const blogs = await helper.blogsInDb()
+    const delBlog = blogs[0]
 
-  assert(Object.keys(blogs[0]).includes('id'))
+    await api.delete(`/api/blogs/${delBlog.id}`).expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    const contents = blogsAtEnd.map(b => b.url)
+
+    assert(!contents.includes(delBlog.url))
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+  })
+
+  test('trying to delete with faulty id returns 400 and does not affect database', async () => {
+    await api.delete('/api/blogs/asdasdasd').expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+  })
 })
 
-test('a valid blog entry is present in the database', async () => {
-  const newBlog = {
-    title: 'testBook',
-    author: 'testAuthor',
-    url: 'test_url',
-    likes: 0
-  }
+describe('PUT', () => {
+  test('modifying works and returns status code 201', async () => {
+    const blogs = await helper.blogsInDb()
+    const modBlog = blogs[0]
+    const ogLikes = modBlog.likes
+    modBlog.likes += 1
+    await api
+      .put(`/api/blogs/${modBlog.id}`)
+      .send(modBlog)
+      .expect(201)
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    assert.strictEqual(modBlog.likes, ogLikes + 1)
+  })
+  test('modifying does not add objects with faulty id and returns 400', async () => {
+    const newBlog = {
+      title: 'Not supposed to be in DB',
+      author: 'Not supposed to be in DB',
+      url: 'Not supposed to be in DB',
+    }
+    await api
+      .put('/api/blogs/asdasdasd')
+      .send(newBlog)
+      .expect(400)
 
-  const blogsAtEnd = await helper.blogsInDb()
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
-
-  const latestEntry = blogsAtEnd.slice(-1)[0]
-
-  assert.strictEqual(latestEntry.title, newBlog.title)
-  assert.strictEqual(latestEntry.author, newBlog.author)
-  assert.strictEqual(latestEntry.url, newBlog.url)
-  assert.strictEqual(latestEntry.likes, newBlog.likes)
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+  })
 })
 
-test('a blog entry with no likes-field is present in the database with likes-value set at 0', async () => {
-  const newBlog = {
-    title: 'testBook',
-    author: 'testAuthor',
-    url: 'test_url',
-  }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+describe('MISCELLANEOUS', () => {
+  test('id field within blogs is valid', async () => {
+    const blogs = await helper.blogsInDb()
 
-  const blogsAtEnd = await helper.blogsInDb()
-  const latestEntry = blogsAtEnd.slice(-1)[0]
-  assert.strictEqual(latestEntry.likes, 0)
-})
-
-test('a blog entry with no title or no url returns 400', async () => {
-  const newBlog = {
-    author: 'testAuthor',
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+    assert(Object.keys(blogs[0]).includes('id'))
+  })
 })
 
 after(async () => {
